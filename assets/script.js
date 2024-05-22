5609fead8611812683227d7a12f12574e6e48c89
William Ndubuisi <princewill835@gmail.com>
2024-05-22 08:34:31 +0100

    hamburger fixed

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,22 @@ window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0)
 })
-const hamMenu = document.querySelector('.ham-menu')
-const offScreenMenu = document.querySelector(".links")
+const hamMenu = document.querySelector('.ham-menu');
+const offScreenMenu = document.querySelector(".links");
+const menuLinks = document.querySelectorAll(".links a");
+
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active')
-})
+    offScreenMenu.classList.toggle('active');
+});
+
+// Function to close the menu
+function closeMenu() {
+    hamMenu.classList.remove('active');
+    offScreenMenu.classList.remove('active');
+}
+
+// Add event listeners to each link
+menuLinks.forEach(link => {
+    link.addEventListener('click', closeMenu);
+});
