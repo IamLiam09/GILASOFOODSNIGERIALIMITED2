d79cf5cfd8a785bdeaa014b47814bba457e8cef3
William Ndubuisi <princewill835@gmail.com>
2024-06-20 00:54:27 +0100

    hamburger on popup

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,29 @@
-// popup
-window.addEventListener("load", function() {
+// temp
+window.addEventListener("load", function () {
     // Disable scrolling by setting overflow to hidden
     document.body.style.overflow = "hidden";
-    
+
     setTimeout(function open(event) {
-        document.querySelector(".popup").style.display = "block";
-    }, 1000);
+        document.querySelector(".temp").style.display = "block";
+        // Scroll to the top of the page(temp)
+        window.scrollTo(0, 0);
+    }, 100);
+
 });
 
-document.querySelector("#close").addEventListener("click", function() {
-    document.querySelector(".popup").style.display = "none";
-    
+document.querySelector("#close").addEventListener("click", function () {
+    document.querySelector(".temp").style.display = "none";
+
     // Enable scrolling by setting overflow to auto
     document.body.style.overflow = "auto";
+    // disabling hamburger menu for mobile
+    hamMenu.addEventListener('click', () => {
+        hamMenu.classList.toggle('active');
+        offScreenMenu.classList.toggle('active');
+    });
 });
 
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0)
 })
@@ -23,10 +31,7 @@ const hamMenu = document.querySelector('.ham-menu');
 const offScreenMenu = document.querySelector(".links");
 const menuLinks = document.querySelectorAll(".links a");
 
-hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
-});
+
 
 // Function to close the menu
 function closeMenu() {
@@ -42,9 +47,9 @@ menuLinks.forEach(link => {
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
-        if(entry.isIntersecting){
+        if (entry.isIntersecting) {
             entry.target.classList.add('show')
-        } else{
+        } else {
             entry.target.classList.remove('show')
         }
     })
